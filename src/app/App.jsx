93a36ffc01f57b1c397e55d2c4325d8c8f280544
pavel_dunin <pasha.dunin.94@gmail.com>
2018-01-01 16:49:03 +0600

    Initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const hello = () => {
+    return (
+        <div>
+            <h1>Hello from my react component!</h1>
+        </div>
+    );
+};
+
+export default hello;
